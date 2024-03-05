7838323ff26a618b35952243beddb2b0a2d6a535
Hazem Sabry <89419013+HazemSabry@users.noreply.github.com>
2024-03-05 22:56:33 +0200

    convert string to array of strings

--- Simple_Shell.c
+++ Simple_Shell.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
 #define MAX_INPUT_LENGTH 100
 
@@ -7,12 +8,26 @@ int main(void){
 	while(1){
 		char cwd[128];
 		char arg[MAX_INPUT_LENGTH];
+		char* token;
+		char* args[10];
+		int counter = 0;
+
 		if(getcwd(cwd, sizeof(cwd)) == NULL){
 			printf("error in ditermin the current work directory\n");
 			return 1;
 		};
 		printf("~%s$ ",cwd);
+
 		fgets(arg, MAX_INPUT_LENGTH, stdin);
+		token = strtok(arg, " ");
+		while(token != NULL){
+			args[counter] = token;
+			token = strtok(NULL, " ");
+			counter++;
+		}
+
+		//for(int i = 0; i < counter; i++){printf("%s\n",args[i]);}
+
 		int pid = fork();
 		if(pid < 0){
 			printf("error in the fork stage/n");
