f9d3bf3829d2fa494ce2a0791545338c0e814bba
Hazem Sabry <89419013+HazemSabry@users.noreply.github.com>
2024-03-05 20:02:43 +0200

    edit the c code

--- Simple_Shell.c
+++ Simple_Shell.c
@@ -1,14 +1,16 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main(void){
+void main(void){
 	while(1){
 		char cwd[128];
 		char c;
 		if(getcwd(cwd, sizeof(cwd)) == NULL){
-			printf("error");
+			printf("error/n");
+			return;
 		};
 		printf("~%s$ ",cwd);
 		scanf("%c",&c);
 	}
+	return;
 }
