30348aa97e1cfb77c9ca0bd4dba6c8049a42ec38
Hazem Sabry <89419013+HazemSabry@users.noreply.github.com>
2024-03-05 21:08:05 +0200

    take input from the user to my shell

--- Simple_Shell.c
+++ Simple_Shell.c
@@ -1,16 +1,29 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void main(void){
+#define MAX_INPUT_LENGTH 100
+
+int main(void){
 	while(1){
 		char cwd[128];
-		char c;
+		char arg[MAX_INPUT_LENGTH];
 		if(getcwd(cwd, sizeof(cwd)) == NULL){
-			printf("error/n");
-			return;
+			printf("error in ditermin the current work directory\n");
+			return 1;
 		};
 		printf("~%s$ ",cwd);
-		scanf("%c",&c);
+		fgets(arg, MAX_INPUT_LENGTH, stdin);
+		int pid = fork();
+		if(pid < 0){
+			printf("error in the fork stage/n");
+			return 1;
+		}else if(pid == 0){
+			//Child Process
+			return 0;
+		}else{
+			//Parent Process
+			
+		}
 	}
-	return;
+	return 0;
 }
